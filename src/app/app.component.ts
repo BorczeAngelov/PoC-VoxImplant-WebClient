9c5bed02c27c2a9a0b1d50125aa5ecf1462c1d56
Borcze Angelov <Borcze.Angelov@yahoo.com>
2024-03-29 19:13:33 +0100

    Add Vercel Analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { VoxImplantLightCallerComponent } from './voxImplant/vox-implant-light-caller/vox-implant-light-caller.component';
 import { VoxImplantTranscriptComponent } from './voxImplant/vox-implant-transcript/vox-implant-transcript.component';
+import { inject } from '@vercel/analytics';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,10 @@ import { VoxImplantTranscriptComponent } from './voxImplant/vox-implant-transcri
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'poc-voxImplant-webClient';
+
+  ngOnInit() {
+    inject();
+  }
 }
